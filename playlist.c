50df58057571b414faefdc80a2c288bce7794545
Naomi Steinmetz <nsteinmetz50@stuy.edu>
2025-01-17 07:08:53 -0500

    shuffle playlist

--- playlist.c
+++ playlist.c
@@ -209,3 +209,25 @@ void remove_song(char * filename){
   curr_list = write_to_playlist(filename, curr_list);
   printf("Removing %s by %s...\n", title, artist);
 }
+
+void shuffle_playlist(char * filename){
+  char line[256];
+  char title[256];
+  char artist[256];
+
+  struct song_node * curr_list = read_from_playlist(filename);
+  struct song_node * new_list = NULL;
+  struct song_node * curr;
+  if (curr_list == NULL){
+    printf("Playlist currently empty. Please go to edit to add songs.\n");
+    return;
+  }
+
+  while (curr_list != NULL){
+    curr = random_finder(curr_list);
+    new_list = insert_front(new_list, curr->artist, curr->title, curr->album, curr->genre, curr->year);
+    curr_list = remove_node(curr_list, curr->artist, curr->title);
+  }
+
+  new_list = write_to_playlist(filename, new_list);
+}
